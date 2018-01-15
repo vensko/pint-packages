fef9648d3c8239a61f98268ff47207dbf8a99e26
vensko <github.com@belbit.net>
2018-01-16 00:34:42 +0100

    PortableApps.com parser

---
+++ scripts/import-portableapps.com.ps1
@@ -0,0 +1,75 @@
+$userAgent = 'PintBot/1.0 (+https://github.com/vensko/pint)'
+
+$client = (new-object Net.WebClient)
+$client.Headers['User-Agent'] = $userAgent
+
+$pahost = 'https://portableapps.com/';
+$url = $pahost + 'apps';
+$content = $client.DownloadString($url) -replace '<!--(.+?)-->',''
+$matches = [regex]::Matches($content, 'apps/\w+/([\w_-]+?)[_-]portable', 'IgnoreCase')
+$target = $PSScriptRoot + '\..\portableapps.com.ini'
+
+Clear-Content $target
+
+$done = @()
+
+foreach ($match in $matches) {
+
+	$id = $match.groups[1].value.replace('_', '-') + '-pa'
+	$uri = $match.groups[0].value
+	$url = $pahost + $uri
+
+	if ($done -contains $id) {
+		continue
+	}
+
+	write-host $uri.padright(50, ' ') ' ' -nonewline
+
+	try {
+		$appContent = $client.DownloadString($url);
+
+		if ($appContent -match '[\d\.]+\+[\d\.]+\w+ download' -or $appContent -like '*online installer*') {
+		    write-host 'ONLINE INSTALLER' -f red
+		    continue
+		}
+
+		$link = [regex]::Matches($appContent, '[^"]+?\.paf\.exe') |% { $_.groups[0].value } | select -first 1
+
+		if (!$link) {
+		    write-host 'LINK NOT FOUND' -f red
+		    continue
+		}
+
+		if ($link.startswith('/')) {
+			$link = $pahost.trim('/') + $link
+		}
+
+		$req = [Net.WebRequest]::Create($link)
+		$req.Timeout = 50000
+		$req.UserAgent = $userAgent
+		$res = $req.GetResponse()
+		$res.close()
+
+		if ($res.Headers['Content-Type'].contains('text/html')) {
+			write-host 'HTML page' -red
+			continue
+		}
+
+		write-host 'OK' -f green
+
+		$ini = @"
+[$id]
+dist = $url
+link = .paf.exe
+keep = data
+
+"@
+
+		$ini | out-file $target -append -encoding ascii
+
+		$done += @($id)
+
+	} catch {
+		write-host $_.Exception.InnerException.Message -f red
+	}
+}
